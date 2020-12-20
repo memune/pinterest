85f61dc816f6823e8bed0d8a96b2a93b929d3f29
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2020-12-20 17:55:05 +0900

    made base templates

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse('안녕하세요.')
-
+    return render(request, 'base.html')
 
 
 
