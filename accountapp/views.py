fafd526f7f8290144bc52937026062bb1beaa15a
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2020-12-20 18:56:13 +0900

    made include, extends format

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
 
 
 
