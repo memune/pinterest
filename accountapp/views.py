ea3ce224cc90b2839bdc662d4f59bf0e46b72078
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2020-12-27 11:50:42 +0900

    create loginacctountview

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -10,6 +13,7 @@ def hello_world(request):
     if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
+
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
@@ -19,6 +23,12 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
-
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    # reverse_lazy는 클래스형 뷰에서 사용함
+    # 작성이 성공했을 때 연동할 url
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
 
 
